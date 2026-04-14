3dc167c704bd53c0d48c20832bc23a66d52ce467
Daniel <daniel.asdahl@icloud.com>
2026-04-14 17:09:06 +0200

    ny-bruker forms med api

---
+++ app/pages/ny-bruker.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="p-6">
+        <h1>Create User</h1>
+
+        <form @submit.prevent="createUser">
+            <input v-model="form.navn" placeholder="Name" />
+            <input v-model="form.epost" placeholder="Email" />
+            <input v-model="form.password" type="password" placeholder="Password" />
+            <select v-model="form.lag_id">
+                <option v-for="lag in lagList" :key="lag.id" :value="lag.id">
+                {{ lag.navn }}
+                </option>
+            </select>
+
+            <button type="submit">Create</button>
+        </form>
+
+        <p v-if="message">{{ message }}</p>
+    </div>
+</template>
+<script setup>
+    const { data: lagList } = await useFetch('/api/lag')
+
+const form = ref({
+  navn: '',
+  epost: '',
+  password: ''
+})
+
+const message = ref('')
+
+const createUser = async () => {
+  try {
+    const res = await $fetch('/api/users', {
+      method: 'POST',
+      body: form.value
+    })
+
+    message.value = 'User created!'
+
+    // clear form
+    form.value = {
+      navn: '',
+      epost: '',
+      password: ''
+    }
+
+  } catch (err) {
+    message.value = err.data?.statusMessage || 'Error'
+  }
+}
+</script>
