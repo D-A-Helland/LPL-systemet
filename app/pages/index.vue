5d5b7d30d5a0438baefd92078bdd673af2393cca
David Aleksander Helland <davidhelland06@gmail.com>
2026-04-17 15:22:06 +0200

    Logout
    NavBar oppdaterer seg ikke til riktige linker etter innlogging/utlogging før refresh.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+const { data: user } = await useFetch('/api/me')
+</script>
+
 <template>
   <UContainer class="flex flex-col justify-center items-center min-h-[70vh]">
     <h1 class="text-2xl font-bold mb-5 text-center">Velkommen til LPL-Systemet!</h1>
     <Priser />
+    <h1 class="text-2xl font-bold">{{ user?.navn }}</h1>
   </UContainer>
 </template>
