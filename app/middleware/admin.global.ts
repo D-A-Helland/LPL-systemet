d589f2c469ee9ecb52095ff9e6102afc35301402
Daniel <daniel.asdahl@icloud.com>
2026-05-06 11:49:53 +0200

    Flyttet ting tilbake i app

--- app/middleware/admin.global.ts
+++ app/middleware/admin.global.ts
@@ -1,18 +1,14 @@
 export default defineNuxtRouteMiddleware(async (to) => {
     if (!to.path.startsWith('/admin')) return
 
-    console.log('ADMIN CHECK')
 
     try {
         const user = await $fetch('/api/auth/me')
-
-        console.log('USER:', user)
 
         if (user.rolle !== 'admin') {
             return navigateTo('/')
         }
     } catch (e) {
-        console.log('NOT LOGGED IN')
         return navigateTo('/login')
     }
 })
